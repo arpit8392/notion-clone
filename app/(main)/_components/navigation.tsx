071b6334220227e9eccfe0668f239ec474782efc
Arpit Namdev <arpit8392@gmail.com>
2023-10-15 14:54:31 +0530

    Search Functionality

--- app/(main)/_components/navigation.tsx
+++ app/(main)/_components/navigation.tsx
@@ -22,6 +22,7 @@ import {
 	PopoverContent,
 	PopoverTrigger,
 } from '@/components/ui/popover'
+import { useSearch } from '@/hooks/use-search'
 import DocumentList from './document-list'
 import { Item } from './item'
 import TrashBox from './trash-box'
@@ -29,6 +30,7 @@ import UserItem from './user-item'
 
 const Navigation = () => {
 	const router = useRouter()
+	const search = useSearch()
 	const pathname = usePathname()
 	const params = useParams()
 	const create = useMutation(api.documents.create)
@@ -147,7 +149,7 @@ const Navigation = () => {
 				</div>
 				<div>
 					<UserItem />
-					<Item label='Search' icon={Search} isSearch onClick={() => {}} />
+					<Item label='Search' icon={Search} isSearch onClick={search.onOpen} />
 					<Item label='Settings' icon={Settings} onClick={() => {}} />
 					<Item label='New Page' icon={PlusCircle} onClick={handleCreate} />
 				</div>
